6e4809ac8f27ba848b3933857f87254afd8e36f7
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2017-02-07 15:34:27 +0100

    ENH: Update docgen to deduplicate .js files for help

--- docs.sh
+++ docs.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+# This doc generation script depends on two things:
+# 1. Checked-out kupfer in a sibling directory
+# 2. Installed kupfer in /usr for the translated help pages
+
+set -x
 set -e
 
 KUPFER=../kupfer
 
 rm -rf Documentation/
 rm -rf help
-#cp -r ../kupfer/Documentation .
-#cp -r ../kupfer/help .
 
 git --git-dir="$KUPFER/.git" archive HEAD help Documentation \
 	| tar xf - --exclude=wscript
@@ -21,7 +24,17 @@ git add -A *.html
 ({
 cd help
 make html
+
+# deduplicate js files
+for file in $(find . -name "*.html") ; do
+	sed -i -e 's|src="\([[:alpha:].]*\)\.js"|src="../C/\1.js"|g' $file
+done
+
+# remove others
+rm [a-z][a-z]/*.js
+
 cp ../helpredirect.html index.html
 git add -A .
+
 })
 
